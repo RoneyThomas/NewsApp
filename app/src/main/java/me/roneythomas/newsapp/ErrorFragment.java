938b6e7a5fff9248f2baed89ea1fbbbd71ec0110
RoneyThomas <roneythomas6@gmail.com>
2016-07-16 02:49:32 -0400

    Removed logging

--- app/src/main/java/me/roneythomas/newsapp/ErrorFragment.java
+++ app/src/main/java/me/roneythomas/newsapp/ErrorFragment.java
@@ -2,14 +2,10 @@ package me.roneythomas.newsapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * Created by roneythomas on 2016-07-16.
- */
 public class ErrorFragment extends Fragment {
     public static ErrorFragment newInstance() {
 
@@ -23,7 +19,6 @@ public class ErrorFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.error_layout, container, false);
-        Log.d("dfssdf","i am working");
         return view;
     }
 }
